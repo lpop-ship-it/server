d32b852149c6a483ddbbc5c483779d4e8cad856a
lpop-ship-it <080907lpop@gmail.com>
2026-01-29 19:01:32 +0300

    шаблон плагина

--- pizdaty_server/start.bat
+++ pizdaty_server/start.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-"C:\Program Files\Eclipse Adoptium\jdk-17.0.17.10-hotspot\bin\java.exe" -Xmx2G -jar server.jar nogui
+"C:\Program Files\Eclipse Adoptium\jdk-25.0.1.8-hotspot\bin\java.exe" -Xmx2G -jar server.jar nogui
 pause
